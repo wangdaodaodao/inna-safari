2dc0cd06a1b47f2f5243064a6131a019423d20ae
wangdaodaodao <ahking0516@126.com>
2024-10-29 18:41:40 +0800

    Initial Commit

---
+++ inna@safari/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  inna@safari
+//
+//  Created by 王导导 on 10/29/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Override point for customization after application launch.
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
---
+++ inna@safariTests/inna_safariTests.swift
@@ -0,0 +1,17 @@
+//
+//  inna_safariTests.swift
+//  inna@safariTests
+//
+//  Created by 王导导 on 10/29/24.
+//
+
+import Testing
+@testable import inna_safari
+
+struct inna_safariTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
